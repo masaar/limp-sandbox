f208e17a814cc81959f016dac531e1c900c7c528
Mahmoud Abduljawad <mahmoud@masaar.com>
2019-02-19 09:12:49 +0400

    -Updated anon token.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
 	production: false,
 	ws_api: 'ws://localhost:8081/ws',
 	http_api: 'http://localhost:8081',
-	anon_token: '__ANON'
+	anon_token: '__ANON_TOKEN_f00000000000000000000012'
 };
 
 /*
