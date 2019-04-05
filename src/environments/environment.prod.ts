effba0def1db4b1588c99aa46a323283debe4370
Mahmoud Abduljawad <mahmoud@masaar.com>
2019-04-05 19:02:53 +0400

    -Updated token for prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	ws_api: 'ws://localhost:8081/ws',
 	http_api: 'http://localhost:8081',
-	anon_token: '__ANON'
+	anon_token: '__ANON_TOKEN_f00000000000000000000012'
 };
